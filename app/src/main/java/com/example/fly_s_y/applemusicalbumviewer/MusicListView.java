f6ae0082075bf19f02529f81d8ff005a85cfd74c
FireLemons <shenyang64@gmail.com>
2018-10-05 02:57:22 -0500

    Load screen dismisses after app refreshes

--- app/src/main/java/com/example/fly_s_y/applemusicalbumviewer/MusicListView.java
+++ app/src/main/java/com/example/fly_s_y/applemusicalbumviewer/MusicListView.java
@@ -2,12 +2,24 @@ package com.example.fly_s_y.applemusicalbumviewer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MusicListView extends AppCompatActivity {
+
+    private View loadScreen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music_list_view);
+
+        loadScreen  = findViewById(R.id.loadScreen);
+    }
+
+    @Override
+    protected void onResume() {
+        loadScreen.setVisibility(View.VISIBLE);
+        super.onResume();
+        loadScreen.setVisibility(View.GONE);
     }
 }
