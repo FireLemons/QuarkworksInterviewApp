bec78b37a49e78e0756d7286048e495b50e0bcf5
FireLemons <shenyang64@gmail.com>
2018-10-23 15:14:32 -0500

    album model class now observable

--- app/src/main/java/com/example/fly_s_y/applemusicalbumviewer/Album.java
+++ app/src/main/java/com/example/fly_s_y/applemusicalbumviewer/Album.java
@@ -1,11 +1,13 @@
 package com.example.fly_s_y.applemusicalbumviewer;
 
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
 import android.graphics.Bitmap;
 
 /**
  * Class representing a single album
  */
-public class Album {
+public class Album extends BaseObservable {
     private String albumName, artistName;
     private Bitmap albumImage;
 
@@ -15,19 +17,23 @@ public class Album {
         this.albumImage = albumImage;
     }
 
+    @Bindable
     public String getAlbumName(){
         return albumName;
     }
 
+    @Bindable
     public String getArtistName(){
         return artistName;
     }
 
+    @Bindable
     public Bitmap getAlbumImage() {
         return albumImage;
     }
 
     public void setAlbumImage(Bitmap albumImage) {
         this.albumImage = albumImage;
+        notifyPropertyChanged(BR.albumImage);
     }
 }
