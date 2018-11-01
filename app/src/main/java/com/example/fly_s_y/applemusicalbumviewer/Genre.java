283302ddc382b294c97cd658055c4aa40af47556
FireLemons <shenyang64@gmail.com>
2018-11-01 05:39:02 -0500

    finished Genre class

--- app/src/main/java/com/example/fly_s_y/applemusicalbumviewer/Genre.java
+++ app/src/main/java/com/example/fly_s_y/applemusicalbumviewer/Genre.java
@@ -1,5 +1,21 @@
 package com.example.fly_s_y.applemusicalbumviewer;
 
+/**
+ * A music genre
+ */
 public class Genre {
+    private String name, link;
 
+    public Genre(String name, String link){
+        this.name = name;
+        this.link = link;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLink() {
+        return link;
+    }
 }
